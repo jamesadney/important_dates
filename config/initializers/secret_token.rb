b60a868326b4f03bb5cd39c731dced215b893c49
James Adney <jfadney@gmail.com>
2013-05-15 13:26:59 -0700

    base app with rspec, capybara gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImportantDates::Application.config.secret_token = '8e78ade8331a5903aaf5d39d24bff76fa83502cf4ae82758654874841ff6e9145657fdd581ac50796fe9d23b2b5bf2b07aaf8d7452f55821b1858a780de186da'
