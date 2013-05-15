36eda53cfb2f59d0688de6083cec2fe7087c15da
James Adney <jfadney@gmail.com>
2013-05-15 16:39:50 -0700

    add pending features for viewing events

---
+++ spec/integration/viewing_events_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'Viewing Events' do
+
+  before do
+    visit '/'
+  end
+
+  scenario "can view events on page"
+  scenario "events are shown with most recent events first"
+  scenario "only upcoming events are shown"
+
+end
